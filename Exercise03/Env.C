46b20a24d33cad721735d6281b7ae0c05d962684
Simon Wessling <simon.wessling@tuhh.de>
2016-06-08 14:21:54 +0200

    kleinere Korrekturen an Env.C/Env.H und an der TypeException. TCTest.C an C++ Syntax angepasst

--- Exercise03/Env.C
+++ Exercise03/Env.C
@@ -1,20 +1,21 @@
 #include "Env.H"
 #include "TypeException.H"
-
+#include "Absyn.H"
 
 using namespace std;
 
-Type Env::lookupVar(String id) {
+Type& Env::lookupVar(string id) {
   unordered_map<string,Type>::iterator it;
 
-  it = m_context.back.find(id);
+  // Find variable at the last element ("top of the stack")
+  it = m_context.back().find(id);
   if (it == m_context.end()) {
     //ERROR no var with this name exists
   }
   return it->second;
 }
 
-FunType Env::lookupFun(String id) {
+FunType Env::lookupFun(string id) {
   unordered_map<string,FunType>::iterator it;
   it = m_fun_context.find(id);
   if (it == m_fun_context.end()) {
@@ -23,14 +24,15 @@ FunType Env::lookupFun(String id) {
   return it->second;
 }
 
-void Env::updateVar(String id, Type ty) {
-  pair<map<string, Type>::iterator, bool> ret;
-  ret = m_context.insert(pair<string, Type>(id, ty));
+void Env::updateVar(string id, Type& ty) {
+  pair<unordered_map<string, Type&>::iterator, bool> ret;
+  // insert to last list item ("top of stack") a new pair (symbol table)
+  ret = m_context.back().insert(pair<string, Type&>(id, ty));
   if (ret.second == false) {
     //ERROR var with id already exist
   }
 }
 
-void Env::updateFun(String id, Type ty) {
+void Env::updateFun(String id, FunType& ty) {
   // TODO
 }
