b9c8cc5307bbd5bc54b631d0acaf08595465481a
Sascha Marcel Hacker <smhrambo@googlemail.com>
2016-06-16 04:55:31 +0200

    env ready

--- Exercise03/Env.C
+++ Exercise03/Env.C
@@ -2,59 +2,72 @@
 
 using namespace std;
 
-BasicType Env::lookupVar(Id id){
-  map<Id, BasicType>::iterator it;
-  // Find variable at the last element ("top of the stack")
-  it = m_context.back().find(id);
-  if (it == m_context.back().end()) {
+Env::Env() {
+	m_context = new list<map<Id, BasicType>* >();
+	m_fun_context = new multimap<Id, FunType*>();
+}
+
+Env::~Env() {
+	clearEnv();
+	delete(m_context);
+	delete(m_fun_context);
+}
+
+BasicType Env::lookupVar(Id id) {
+  auto it = m_context->back()->find(id);
+  if (it == m_context->back()->end()) {
     throw new TypeException("Variable " + id + " was not declared.");
   }
   return it->second;
 }
 
-BasicType Env::lookupFun(Id id, vector<BasicType>* arglist){
-  map<Id, FunType>::iterator it;
-  it = m_fun_context.find(id);
-  // TODO Do we need to check of the exact signature matches?
-  if (it == m_fun_context.end()) {
+BasicType Env::lookupFun(Id id, vector<BasicType>* arglist) {
+	auto ret = m_fun_context->equal_range(id);
+	for(auto item = ret.first; item != ret.second; item++) {
+		if(equal(arglist->begin(), arglist->end(), item->second->fun_args.begin(), item->second->fun_args.end())) {
+			return item->second->fun_type;
+		}
+	}
     throw new TypeException("Function " + id + " was not declared.");
-  }
-  return it->second.getFunReturnType();
 }
 
 void Env::updateVar(Id id, BasicType ty) {
-  pair<map<Id, BasicType>::iterator, bool> ret;
-  // insert to last list item ("top of stack") a new pair (symbol table)
-  ret = m_context.back().insert(pair<Id, BasicType>(id, ty));
+  auto ret = m_context->back()->insert(pair<Id, BasicType>(id, ty));
   if (ret.second == false) {
 	  throw new TypeException("Variable" + id + " already exists");
   }
 }
 
-void Env::updateFun(Id id, FunType ty) {
-    pair<map<Id, FunType>::iterator, bool> ret;
-    ret = m_fun_context.insert(std::pair<Id, FunType>(id, ty));
-    if (ret.second == false) {
-      throw new TypeException("Function " + id + " already exists.");
-    }
+void Env::updateFun(Id id, FunType* ty) {
+	auto ret = m_fun_context->equal_range(id);
+	for(auto item = ret.first; item != ret.second; item++) {
+		if(equal(ty->fun_args.begin(), ty->fun_args.end(), item->second->fun_args.begin(), item->second->fun_args.end())) {
+		    throw new TypeException("Function " + id + " already declared.");
+		}
+	}
+	m_fun_context->insert(pair<Id, FunType*>(id, ty));
 }
 
 void Env::addScope() {
-  map<Id, BasicType>* tmp = new map<Id, BasicType>();
-	m_context.push_back(*tmp); 
-  delete tmp; // push_back is call-by-value, therefore we can free tmp
+	m_context->push_back( new map<Id, BasicType>());
 }
 
 void Env::delScope() {
-	if(!m_context.empty()) {
-		m_context.pop_back();
+	if(!m_context->empty()) {
+		delete(m_context->back());
+		m_context->pop_back();
 	}
 }
 
 void Env::clearEnv() {
-	for(auto item : m_context) {
-		delete(item);
-		m_context.erase(item++);
-
+	for(auto item = m_context->begin(); item != m_context->end();) {
+		delete(*item);
+		m_context->erase(item++);
 	}
+
+	for(auto item = m_fun_context->begin(); item != m_fun_context->end();) {
+		delete(item->second);
+		m_fun_context->erase(item++);
+	}
+
 }
