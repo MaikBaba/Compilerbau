8e932a9dce95477a6d7ca17ceac9c12dc05bebfe
Sascha Marcel Hacker <smhrambo@googlemail.com>
2016-06-20 00:41:05 +0200

    add data

---
+++ Exercise04/TypeException.C
@@ -0,0 +1,12 @@
+#include "TypeException.H"
+
+TypeException::TypeException(string msg){
+  m_msg = msg;
+}
+
+TypeException::~TypeException() throw(){
+}
+
+string TypeException::what() throw(){
+  return m_msg;
+}
